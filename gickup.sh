ca34ae7035b6c6fadc7e3fa2caba88620fd7aff1
Jeff Geerling <geerlingguy@mac.com>
2021-12-02 13:06:43 -0600

    Issue #2: Get symlinks working in CIFS mounts.

--- gickup.sh
+++ gickup.sh
@@ -6,4 +6,16 @@
 #   ./gickup.sh
 #
 # Script requires valid credentials - set up following gickup's README.
-TODO.
+
+# TODO: Run this command if the known_hosts file doesn't already exist.
+# ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+GICKUP=/usr/local/bin/gickup
+
+# Check if gickup is installed.
+if ! [ -x "$(command -v $GICKUP)" ]; then
+  echo 'Error: gickup is not installed.' >&2
+  exit 1
+fi
+
+$GICKUP ~/.gickup.yml
