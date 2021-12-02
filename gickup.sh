dd4830925ecdbfdd962792fbdb86d97db1239332
Jeff Geerling <geerlingguy@mac.com>
2021-12-02 15:06:01 -0600

    Issue #2: Updates for latest version of Gickup.

--- gickup.sh
+++ gickup.sh
@@ -5,12 +5,32 @@
 # Basic usage:
 #   ./gickup.sh
 #
-# Script requires valid credentials - set up following gickup's README.
-
-# TODO: Run this command if the known_hosts file doesn't already exist.
-# ssh-keyscan github.com >> ~/.ssh/known_hosts
+# Script requires valid credentials - set up following gickup's README, in the
+# format below:
+#
+# ```
+# source:
+#   github:
+#     - token: [redacted]  # generate an personal access token in dev settings
+#       user: geerlingguy
+#       username: geerlingguy
+#       ssh: true
+#       sshkey: /home/pi/.ssh/id_ed25519  # must be added to your GitHub account
+#       exclude:
+#         - linux
+# destination:
+#   local:
+#     - path: "/Volumes/Git-Backups"
+# ```
 
 GICKUP=/usr/local/bin/gickup
+KNOWN_HOSTS=~/.ssh/known_hosts
+
+# Make sure a known_hosts file exists.
+if [ ! -f "$KNOWN_HOSTS" ]; then
+  echo 'Generating known_hosts file.'
+  ssh-keyscan github.com >> $KNOWN_HOSTS
+fi
 
 # Check if gickup is installed.
 if ! [ -x "$(command -v $GICKUP)" ]; then
