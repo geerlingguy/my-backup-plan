aa5b4fe89cfc0f5956123f572f86aa211b7a7f5d
Jeff Geerling <geerlingguy@mac.com>
2021-12-01 18:13:44 -0600

    Add in Gickup role and example script.

---
+++ gickup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# gickup script to archive Jeff Geerling's GitHub repositories locally.
+#
+# Basic usage:
+#   ./gickup.sh
+#
+# Script requires valid credentials - set up following gickup's README.
+TODO.
